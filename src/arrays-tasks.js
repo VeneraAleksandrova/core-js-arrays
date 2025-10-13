cbb7c3789693c357df91b32f32ec44defc9ba5ec
Venera Aleksandrova <venera.n.aleksandrova@gmail.com>
2025-10-14 02:24:27 +0300

    feat: add solution

--- src/arrays-tasks.js
+++ src/arrays-tasks.js
@@ -496,8 +496,22 @@ function findCommonElements(arr1, arr2) {
  *    findLongestIncreasingSubsequence([3, 10, 2, 1, 20]) => 2
  *    findLongestIncreasingSubsequence([50, 3, 10, 7, 40, 80]) => 3
  */
-function findLongestIncreasingSubsequence(/* nums */) {
-  throw new Error('Not implemented');
+function findLongestIncreasingSubsequence(nums) {
+  let j = 1;
+  let seq = 0;
+  nums.map((element, i, array) => {
+    if (i > 0 && element > array[i - 1]) {
+      j += 1;
+      if (i === array.length - 1) {
+        seq = Math.max(seq, j);
+      }
+    } else {
+      seq = Math.max(seq, j);
+      j = 1;
+    }
+    return seq;
+  });
+  return seq;
 }
 
 /**
